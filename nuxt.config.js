249fd7382c2e63259bcd195901295b72c220edf7
Nikolay <stkautoru@mail.ru>
2022-12-23 01:35:40 +0700

    nuxt deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,11 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers: https://go.nuxtjs.dev/config-head,
+  server: {
+    host: '0.0.0.0',
+    port: 80
+  },
   head: {
     titleTemplate: '%s - shop-frontend',
     title: 'shop-frontend',
